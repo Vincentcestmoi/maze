fad413c08c61e5e0bc3a8e0ea2e1f7a263924257
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-11-17 18:58:16 +0100

    maze_gen.c -implémentation random_maze_ab -"implémentation" maze_test

--- Sources/maze_gen.c
+++ Sources/maze_gen.c
@@ -7,8 +7,31 @@ void maze_test(maze*) {
     //ne fait rien
 }
 
-void random_maze_ab(maze*) {
-    return;
+void random_maze_ab(maze *p_maze) {
+    bool visited[p_maze->hsize * p_maze->vsize]; // Tableau de booléens pour savoir si une case a été visitée
+    for(int i = 0; i < p_maze->hsize * p_maze->vsize; i++)
+    {
+        visited[i] = false; //aucune case n'a été visitée
+    }
+    int visited_count = p_maze->hsize * p_maze->vsize - 1; //nombre de case à visiter (la case de départ est déjà visitée)
+    int cell = rand() % (p_maze->hsize * p_maze->vsize); //case de départ aléatoire
+    visited[cell] = true; //on visite la case de départ
+    cardinal card = rand() % 4; //direction aléatoire
+    while(visited_count > 0)
+    {
+        const int neigbour = get_adj_maze(p_maze, cell, card); //case voisine
+        if(neigbour !=1)
+        {
+            if(!visited[neigbour]) //si la case voisine n'a pas été visitée, on la visite
+            {
+                visited[neigbour] = true;
+                visited_count--;
+                mask_cell_maze(p_maze, cell); //on casse le mur entre la case actuelle et la case voisine
+            }
+            cell = neigbour; //on se déplace
+        }
+        card = rand() % 4; //nouvelle direction aléatoire
+    }
 }
 
 void random_maze_wilson(maze*) {

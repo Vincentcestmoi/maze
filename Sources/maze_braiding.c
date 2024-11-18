ea37dbe3d9baa00cabe1b722ca93a510045d2cb5
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-11-18 21:38:26 +0100

    maze_braiding.c -localisation bug -conformisme

--- Sources/maze_braiding.c
+++ Sources/maze_braiding.c
@@ -17,13 +17,13 @@ bool is_dead_end(maze* p_maze, const int cell) {
     if (value & 8){
         c++;
     }
-    return c==3;
+    return c == 3;
 }
 
 int count_dead_ends(maze* p_maze) {
     int c = 0;
-    for (int i=0 ; i < p_maze->hsize * p_maze->vsize ; i++){
-        if (is_dead_end(p_maze, i)){
+    for (int i = 0; i < p_maze->hsize * p_maze->vsize; i++){
+        if(is_dead_end(p_maze, i)){
             c++;
         }
     }
@@ -31,10 +31,15 @@ int count_dead_ends(maze* p_maze) {
 }
 
 void remove_one_dead_end(maze* p_maze, const int cell, const int odds) {
+    if(rand() % 100 + 1 < odds){
+        printf("bullshit\n");
+    }
+    fflush(stdout);
+    return;
     bool tab[4] = {0};
     if (is_dead_end(p_maze, cell) && rand() % 100 + 1 < odds){
         for (int i = 0 ; i < 4 ; i++){
-            if (cell & 32 &&
+            if ((p_maze->props[cell] & 32) &&
                 has_wall_maze(p_maze, cell, i) &&
                 (!has_wall_maze(p_maze, cell, (i + 2) % 4) ||
                 (!has_wall_maze(p_maze, cell, (i + 1) % 4 ) && has_wall_maze(p_maze, get_adj_maze(p_maze, cell, (i + 1) % 4), i))||
@@ -44,20 +49,20 @@ void remove_one_dead_end(maze* p_maze, const int cell, const int odds) {
         }
     }
     char c = 0;
-    for (int i=0; i<4 ;i++){
+    for (int i = 0; i < 4 ;i++){
         if (tab[i]){
             c++;
         }
     }
     unsigned char card = rand() % c;
-    while (!has_wall_maze(p_maze, cell, card)){
+    while(!has_wall_maze(p_maze, cell, card)){
         card = (card + 1) % 4;
     }
     del_wall_maze(p_maze, cell, card);
 }
 
 void braid_maze(maze* p_maze, const int odds) {
-    for (int i=0 ; i < p_maze->hsize * p_maze->vsize ; i++){
+    for (int i = 0 ; i < p_maze->hsize * p_maze->vsize ; i++){
         remove_one_dead_end(p_maze, i, odds);
     }
 }
